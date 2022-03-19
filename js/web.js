c014afa453aebd0d83aafe6237dab1b24d06a788
hwangsangsung <tkstjddl83@gmail.com>
2022-03-19 12:07:57 +0900

    변수와 테이터타입

---
+++ js/web.js
@@ -0,0 +1,15 @@
+var count = 100;
+count += 50; // 숫자타입의 변수//
+count += "원"; // 문자타입으로 변경//
+count += 50; // 문자로 계속 나감//
+count = 100; // 150원 50150 //
+var count = "100원";
+var arr = ["회사소개", "제품소개", "고객센터"];
+
+
+
+
+
+
+console.log("count 변수값은? ", count , "데이터타입은 " , typeof count );
+console.log("arr 값을 가져올때? ", arr[0] , "데이터타입은 " , typeof arr );
